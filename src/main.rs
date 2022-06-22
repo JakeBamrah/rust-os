8a64f4f580b6423b8b4eebff9b49b4554898c045
Jake Bamrah <login@jakebamrah.me>
2022-06-22 23:25:30 +0000

    setup vga buffer and config changes for building boot image

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,21 @@ fn panic(_info: &PanicInfo) -> ! {
 }
 
 // overwrite entrypoint inplace of absent rust runtime (no main)
+static HELLO: &[u8] = b"Hello world!";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     // this function is the entry point, since the linker looks for a function
     // named `_start` by default (when using Linux conventions)
+    let vga_buffer = 0xb8000 as *mut u8;
+    // iterate over the bytes of the static string
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            // use offset to write string byte and color byte (0xb)
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop{}
 }
